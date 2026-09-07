e1d9e3d0c0af6231bfc8f11141b823bc0ec936b4
namratanimhan20-alt <namrata.nimhan20@gmail.com>
2026-09-07 22:27:40 +0530

    Updated

---
+++ fundamental_Programs/Program6.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+class Demo { 
+public: 
+    Demo() { 
+        cout << "Constructor called "; 
+    } 
+    ~Demo() { 
+        cout << "Destructor called "; 
+    } 
+}; 
+
+int main() { 
+    Demo d; 
+    return 0; 
+} 
